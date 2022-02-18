2ce6c7e66c9ba043e9c1b25db778b3f0661518ee
Veronica Escobar <vescobars2001@gmail.com>
2022-02-17 20:03:18 -0500

    finish configuration of the main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+        //RUN CONSOLE AND YIELD INFORMATION
+
+        //CREATE THE BUFFERS AND THE PRODUCER/CONSUMERS
+
+        //RETURN RESULTS
+
+        //NEED TO HAVE DATA
+        //BUZON
+            //buffer size
+        //PROCESS
+            //tiempo de demora
+            //estilo de comunicacion (pasivo/activo) (yield o wait)
+            //Proceso 1 es el que sabe cuantos mensajes se deben mandar
+    }
+
+    /**
+     * This method is in charge of parsing the input from the console and starting the threads
+     */
+    public static List parseConsoleInput(){
+        //TODO: Console text. Look at specified format.
+
+        //NEED TO HAVE DATA
+        //BUZON
+        //buffer size
+        //PROCESS
+        //tiempo de demora
+        //estilo de comunicacion (pasivo/activo) (yield o wait)
+        //Proceso 1 es el que sabe cuantos mensajes se deben mandar
+
+        return null;//delete this when finished setting up the methods
+    }
+
+
+    /**
+     * In charge of creating the buffers, the producer-consumers and starting them acording to the given parameters
+     */
+    public static void setUp(){
+        //TODO: Create the entities and start the threads in order
 
         /**
          * BUFFER CREATION
@@ -19,5 +59,8 @@ public class Main {
          */
         //TODO: Una vez todos los threads esten toca incialrizarlos aqui con todos los parametros indicados
 
+
     }
+
+
 }
