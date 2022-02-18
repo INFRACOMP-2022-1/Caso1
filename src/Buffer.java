d5ce584799e3d01f52fc08aed64607afd11d4646
Veronica Escobar <vescobars2001@gmail.com>
2022-02-17 19:44:45 -0500

    Main and buffer class intial implementation finished

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,2 +1,51 @@
+import java.util.List;
+import java.util.LinkedList;
+
 public class Buffer {
+    private List<String> buffer;//Contains the messages that are being sent
+    private int size;//Capacidad del buffer
+
+    public Buffer(int size){
+        this.size = size;
+        this.buffer = new LinkedList<>();
+    }
+
+    public synchronized boolean hasMessages(){
+        return this.buffer.size() > 0;
+    }
+
+    public synchronized void insertMessage(String message){
+        while(this.buffer.size() == this.size){
+            try {
+                wait();//protected wait
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Guardar mensaje
+        this.buffer.add(message);
+
+        //Notificar a los consumidores
+        notify();
+    }
+
+    public synchronized  String retrieveMessages(){
+        while(this.buffer.size() == 0){
+            try{
+                wait();
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        //Retirar el mensaje
+        String message = this.buffer.remove(0);
+        notify();
+
+        //Entregar
+        return message;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args){
+
+        /**
+         * BUFFER CREATION
+         */
+        //Buffer bufferA = new Buffer();
+        //Buffer bufferB = new Buffer();
+        //Buffer bufferC = new Buffer();
+        //Buffer bufferD = new Buffer();
+
+        /**
+         * PRODUCER/CONSUMER CREATION
+         */
+        //TODO: Crear los cuatro productores-consumidores que conectan los buffers
+
+        /**
+         * START THREADS
+         */
+        //TODO: Una vez todos los threads esten toca incialrizarlos aqui con todos los parametros indicados
+
+    }
 }
