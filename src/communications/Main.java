c41a4e974026a1e89f353779f05f3e2603994991
Veronica Escobar <vescobars2001@gmail.com>
2022-02-20 12:36:50 -0500

    Termine de hacer el main, al menos la parte de leer el input y dejar los buffers y producer consumers listos, y deje unos todos para lo que toca hacer en el main method para poner a correr los strings. Aun no estoy segura como se manejara lo del primer thread. Ahorita añadire que se le pida al usaurio que porfavor inputee el numero de mensajes que quiere mandar

--- src/communications/Main.java
+++ src/communications/Main.java
@@ -14,17 +14,51 @@ public class Main {
 
     //TODO ; Terminar configuracion y poner algo para poner a correr los processos y coordinarlos
     //TODO: TERMINAR LA EJECUCION DE LOS PROCESOS ,USAR BARRERASCICLICAS O ALGO
-    public final static String fileName = "../inputFile.txt";
+    public final static String fileName = "Caso1/inputFile.txt";
+
+    public static HashMap<String,Buffer> bufferList;
+
+    public static HashMap<String,ProducerConsumer> producerConsumersList;
+
+
+
+    //TODO : Declare parameters for all the qualities of the buffers and the processes
+    //TODO: These parameters will be initializes by the parseConsole method
+    public static void main(String[] args){
+        //RUN CONSOLE AND RETURN THE RESULTING INFORMATION
+
+        System.out.println("THE CONFIGURATION FOR THE PROCESSES AND THE BUFFERS MUST BE PUT IN THE inputFile.txt WITH THE SPECIFIED PATTERN GIVEN IN THE CASE SPECIFICATIONS ");
+
+        //Parses the input file txt content and creates the producerConsumers and the Buffers
+        parseConsoleInput();
+
+        System.out.println("The input configuration has been completed");
+
+        //TODO: START CYCLIC BARRIER HERE
+
+        //TODO: START THE THREADS
+
+            //TODO: START PROCESS-CONSUMER 1 AND SEND THE INITIAL NUMBER OF MESSAGES
+
+
+            //TODO: START ALL THE OTHER THREADS
+
+        //TODO: JOIN THE THREADS HERE TO KILL THEM
+
+        //TODO: WRITE RESULTS SOMEWHERE?
+    }
+
 
     public static void parseConsoleInput(){
         ArrayList<String> rawInput = readInputFile();
-        HashMap<String,Buffer> bufferList = new HashMap<>();
-        HashMap<String,ProducerConsumer> producerConsumersList = new HashMap<>();
+        bufferList = new HashMap<>();
+        producerConsumersList = new HashMap<>();
 
         //Create the buffers
-        for(int i = 0; i< 3;i++){
+        for(int i = 0; i< 4;i++){
             Buffer newBuffer = createBuffer(rawInput.get(i));
             bufferList.put(newBuffer.getBufferId(),newBuffer);
+            System.out.println(newBuffer.toString());
         }
 
         //Create the processes
@@ -43,13 +77,14 @@ public class Main {
             //TODO: Decide what im going to do about the messages
             ProducerConsumer newProducerConsumer = new ProducerConsumer(originBuffer,destinationBuffer,"",pcId,activeReception,activeEmission, sleepTimeMilli );
             producerConsumersList.put(Long.toString(newProducerConsumer.getPcId()),newProducerConsumer);
+            System.out.println(newProducerConsumer.toString());
         }
     }
 
     public static Buffer createBuffer(String specifications){
-        String[] formatedStr = specifications.split(" ");
-        String bufferId = formatedStr[0];
-        int maxCapacity = Integer.parseInt(formatedStr[1]);
+        String[] formattedStr = specifications.split(" ");
+        String bufferId = formattedStr[0];
+        int maxCapacity = Integer.parseInt(formattedStr[1]);
 
         Buffer buffer = new Buffer(bufferId,maxCapacity);
         return buffer;
